9ce03236471795ce53815a4f5943ed0eaf325d40
TarekM007 <tarekmoustafa36@gmail.com>
2023-12-18 09:26:22 -0800

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
-        count = 0
         for i in range(x):
-            print(my_list[i], end="")
-            count += 1
-        print()
-    except IndexError:
+            try:
+                print(my_list[i], end=" ")
+                count += 1
+            except IndexError:
+                break
+    except TypeError:
         pass
-    return count
+    finally:
+        print()
+        return count
