a3a30a99822a447369bf34b95608340d229b5ce9
TarekM007 <tarekmoustafa36@gmail.com>
2023-12-20 12:57:25 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square module"""
+
+
+class Square:
+    """a square class"""
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
