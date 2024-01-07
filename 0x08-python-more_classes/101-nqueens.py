5c5a521a46eb5106c9b507369e1901d072e78811
TarekM007 <tarekmoustafa36@gmail.com>
2024-01-06 16:04:24 -0800

    101

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -52,24 +52,24 @@ class NQueen:
 
 # Main
 
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+N = sys.argv[1]
+
 try:
-    if len(sys.argv) != 2:
-        raise ValueError("Usage: nqueens N")
-
-    N = int(sys.argv[1])
-    if N < 4:
-        raise ValueError("N must be at least 4")
-
-    queen = NQueen(N)
-    res = queen.nQueen(1)
-
-    for i in res:
-        print(i)
-
-except ValueError as ve:
-    print("Error:", str(ve))
+    N = int(N)
+except:
+    print("N must be a number")
     sys.exit(1)
 
-except Exception as e:
-    print("An unexpected error occurred:", str(e))
+if N < 4:
+    print("N must be at least 4")
     sys.exit(1)
+
+queen = NQueen(N)
+res = queen.nQueen(1)
+
+for i in res:
+    print(i)
