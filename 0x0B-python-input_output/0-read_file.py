e6adcc5873fb4065c5b9a2ce52b64d8a6006431a
TarekM007 <tarekmoustafa36@gmail.com>
2024-01-09 10:31:18 -0800

    0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    reads filename with utf-8
+    """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end="")
