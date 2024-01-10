4cf768a6056fee7c30ab6167ad949cf920e69f0e
TarekM007 <tarekmoustafa36@gmail.com>
2024-01-10 11:52:32 -0800

    4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a module"""
+
+
+def print_square(size):
+    """
+    prints a square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
