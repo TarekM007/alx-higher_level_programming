88d4e99966d022104ef28316cb1dca298754666c
TarekM007 <tarekmoustafa36@gmail.com>
2024-01-10 11:55:10 -0800

    4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,8 +5,11 @@
 def print_square(size):
     """
     prints a square
+
+    Args:
+        size: size length of the square
     """
-    if not isinstance(size, int):
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
