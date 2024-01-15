941a43f0795d29caa02306e00277f9feac2a6009
TarekM007 <tarekmoustafa36@gmail.com>
2024-01-15 12:18:15 -0800

    1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,30 +16,38 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+        @property
         def get_width(self):
             """width of this triangle"""
             return self.__width
 
+        @get_width.setter
         def set_width(self, value):
             self.__width = value
 
+        @property
         def get_height(self):
             """height of this triangle"""
             return self.__height
 
+        @get_height.setter
         def set_height(self, value):
             self.__height = value
 
+        @property
         def get_x(self):
             """x of this triangle"""
             return self.__x
 
+        @get_x.setter
         def set_x(self, value):
             self.__x = value
 
+        @property
         def get_y(self):
             """y of this triangle"""
             return self.__y
 
+        @get_y.setter
         def set_y(self, value):
             self.__y = value
