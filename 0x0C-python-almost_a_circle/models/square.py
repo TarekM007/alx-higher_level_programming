40a8554e8baa56b95cb223a43dcf7b1a2e617e21
TarekM007 <tarekmoustafa36@gmail.com>
2024-01-15 14:17:03 -0800

    7

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" a module"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initiation method"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''Returns string info about this square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width)
