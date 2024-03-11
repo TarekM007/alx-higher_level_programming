e41ab0e141d507bbea2345be26f8296bd603533e
TarekM007 <tarekmoustafa36@gmail.com>
2024-03-11 15:56:47 -0700

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+  const second = arr.sort((a, b) =>  b - a);
+  console.log(second[1]);
 }
