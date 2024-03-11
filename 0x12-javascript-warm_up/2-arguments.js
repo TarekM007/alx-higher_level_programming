1fe55410fb2472a85ea747ffdcce2f783e21f3f2
TarekM007 <tarekmoustafa36@gmail.com>
2024-03-11 11:14:52 -0700

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const argsCount = process.argv.length - 2;
-if (argsCount >= 1) {
+if (argsCount === 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
