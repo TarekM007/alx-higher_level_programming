e04a4ccf6528e67a38c4b0db45b23a06d505043c
TarekM007 <tarekmoustafa36@gmail.com>
2024-03-11 14:53:23 -0700

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 function fact (a) {
-  if (a === 0 || a === 1) {
+  if (isNaN(a)) {
+    return 1;
+  } else if (a === 0) {
     return 1;
   } else {
     return a * fact(a - 1);
   }
 }
 const arg = process.argv[2];
-if (arg === undefined) {
-  console.log('1');
-}
 const intVal = parseInt(arg);
 const result = fact(intVal);
 console.log(result);
